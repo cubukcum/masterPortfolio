90d30a65d7424bc766ed45e728355ee261382e02
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-04-25 22:39:50 +0300

    updated contact me

--- src/portfolio.js
+++ src/portfolio.js
@@ -728,23 +728,23 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "I am available on LinkedIn. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
-  blogSection: {
-    title: "Blogs",
-    subtitle:
-      "I like to document some of my experiences in professional career journey as well as some technical knowledge sharing.",
-    link: "https://blogs.ashutoshhathidara.com/",
-    avatar_image_path: "blogs_image.svg",
-  },
+  // blogSection: {
+  //   title: "Blogs",
+  //   subtitle:
+  //     "I like to document some of my experiences in professional career journey as well as some technical knowledge sharing.",
+  //   link: "https://blogs.ashutoshhathidara.com/",
+  //   avatar_image_path: "blogs_image.svg",
+  // },
   addressSection: {
     title: "Address",
-    subtitle: "Saratoga Ave, San Jose, CA, USA 95129",
-    locality: "San Jose",
-    country: "USA",
-    region: "California",
-    postalCode: "95129",
-    streetAddress: "Saratoga Avenue",
+    subtitle: "Yazir mah",
+    locality: "Selcuklu",
+    country: "Turkiye",
+    region: "Konya",
+    postalCode: "42130",
+    streetAddress: "Yazir mah",
     avatar_image_path: "address_image.svg",
     location_map_link: "https://maps.app.goo.gl/NvYZqa34Wye4tpS17",
   },
@@ -766,6 +766,6 @@ export {
   experience,
   projectsHeader,
   publicationsHeader,
-  publications,
+  // publications,
   contactPageData,
 };
